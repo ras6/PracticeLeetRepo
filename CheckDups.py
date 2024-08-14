d540246d52d659bfd41ec64aaee8307e8ba71e5e
ras6 <31150505+ras6@users.noreply.github.com>
2024-08-14 00:37:14 -0500

    Create CheckDups.py
    Checking Dups in an unsorted array #217

---
+++ CheckDups.py
@@ -0,0 +1,15 @@
+# Checking Duplicates
+# Time: o(n) 
+# Space: O(n)
+
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+      hashset = set()
+
+      for n in nums:
+        if n in hashset:
+          return True
+        hashset.add(n)
+      return False
+        
